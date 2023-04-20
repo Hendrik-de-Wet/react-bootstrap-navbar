4d6dbd1249bfd3929201eb1e9c3ac0a8e96490ad
Hendrik de Wet <hendrik.dewet@gmail.com>
2023-04-21 09:31:40 +1200

    React Framer Motion NavBar initial commit.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+// Import bootstrap styles
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// Import navbar component
+import Header from "./components/Header";
+import { Container, Row, Col } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      {/* Image used to demonstrate react bootstrap navigation bar with framer motion.  */}
+      <div className="content-placeholder">
+        <Container>
+        <Row className="justify-content-md-center">
+            <Col xs lg="8">
+              <h1 style={{ paddingTop: "200px" }}>
+                React Bootstrap Framer Motion Navigation Bar Demonstration
+              </h1>
+              <p>Create a new react application, open a terminal window and enter the line below.</p>
+              <code>npx create-react-app react-bootstrap-navbar</code>
+              <p>Next change to working folder create in the first step.</p>
+              <code>cd react-bootstrap-navbar</code>
+
+              
+              <p>Install the bootstrap library below in the same terminal window.</p>
+              <code>npm install react-bootstrap bootstrap</code>
+              <p><small><i>
+                Read more: <a href="https://react-bootstrap.github.io/getting-started/introduction">https://react-bootstrap.github.io/getting-started/introduction</a>
+                </i></small></p>
+              <p>Don't forget to include the bootstrap styles in your react application.</p>
+              <p>The following line can be included in your src/index.js or App.js file.</p>
+              <code>import 'bootstrap/dist/css/bootstrap.min.css';</code>
+
+              <p>Install the Framer Motion library below.</p>
+              <code>npm install framer-motion</code>
+              <p><small><i>Read more:  <a href="https://www.framer.com/motion/introduction/">https://www.framer.com/motion/introduction/</a> </i></small></p>
+
+              <p>Next build a component and add the relevant styling.</p>
+
+              <p>To start the application, open a new terminal and enter the line below.</p>
+              <code>npm start</code>
+              <p>Good to go!</p>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,116 @@
+import React from "react";
+
+// React Bootstrap library used for navigation menu.
+import { Container, Navbar, NavDropdown, Nav, Button } from "react-bootstrap";
+
+// Framer motion library used for animation.
+import { motion, useScroll } from "framer-motion";
+
+const navStyles = {
+  position: "fixed",
+  alignItems: "center",
+  height: "85px",
+  width: "100%",
+  backgroundColor: "unset",
+};
+
+const navLinksWrapper = {
+  alignItems: "center",
+  width: "100%",
+};
+
+const Header = () => {
+  // This hook gets the scroll y-axis.
+  const { scrollY } = useScroll();
+
+  // This hook manages state.
+  const [scrollAction, setScrollAction] = React.useState(false);
+
+  // This onUpdate function will be called in the `scrollY.onChange` callback.
+  function update() {
+    if (scrollY?.current < scrollY?.prev ) {
+      setScrollAction(false);
+    } else if (scrollY?.current > 50 && scrollY?.current > scrollY?.prev) {
+      setScrollAction(true);
+    }
+  }
+
+  // Update the onChange callback to call for `update()`.
+  React.useEffect(() => {
+    return scrollY.onChange(() => update());
+  });
+
+
+  const variants = {
+    // This is the "after scroll" key and it's correlating styles.
+    afterScroll: {
+      zIndex: 10,
+      opacity: 1,
+      y: -25,
+      backgroundColor: "#FFF",
+      boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",
+      paddingLeft: "10px",
+      paddingTop: "10px",
+    },
+    // This is the "before scroll" key and it's correlating styles.
+    beforeScroll: {
+      zIndex: 10,
+      opacity: 1,
+      y: -10,
+      height: "100px",
+      paddingLeft: "10px",
+      paddingTop: "25px",
+    },
+  };
+
+  return (
+    <motion.nav // The variants object needs to be passed into the motion component.
+      variants={variants}
+      // Match our boolean state with these variant keys.
+      animate={scrollAction ? "afterScroll" : "beforeScroll"}
+      // Add a custom easing curve and duration for the animation.
+      transition={{ ease: [0.1, 0.25, 0.3, 1], duration: 0.4 }}
+      // Basic nav styles.
+      style={navStyles}
+    >
+      <div style={navLinksWrapper}>
+        {/* React Boostrap navigation component.   */}
+        <Navbar collapseOnSelect expand="lg" className="navbar">
+          <Container>
+            <Navbar.Brand href="/" className="navbar-brand">
+              <p className="logo">| LOGO |</p>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav>
+                <Nav.Link href="/">Home</Nav.Link>
+                <NavDropdown title="Menu Item" id="navbarScrollingDropdown">
+                  <NavDropdown.Item href="/">Item 1</NavDropdown.Item>
+                  <NavDropdown.Item href="/">Item 2</NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link href="/">
+                  Menu Item
+                </Nav.Link>
+                <NavDropdown title="Menu Item" id="navbarScrollingDropdown">
+                  <NavDropdown.Item href="/">Item 1</NavDropdown.Item>
+                  <NavDropdown.Item href="/">Item 2</NavDropdown.Item>
+                </NavDropdown>
+              
+              </Nav>
+              <Nav>
+                <Nav.Link href="/">Log In</Nav.Link>
+              </Nav>
+              <Nav>
+                <Nav.Link href="/">
+                  <Button variant="btn btn-outline-secondary">Register</Button>
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    </motion.nav>
+  );
+};
+
+export default Header;
